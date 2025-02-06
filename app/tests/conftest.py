0254183bf8d680dbcd06c2b4398ad817f589e281
Leonardo Comar <leofcomar@gmail.com>
2025-02-05 23:46:03 -0300

    Testando nova configuração

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,10 +1,14 @@
 import pytest
-from app.main import app
+from app.repository.dynamodb_repository import create_users_table
+from app.repository.s3_repository import create_s3_bucket
+from app.repository.email_ses_repository import verify_ses_email_identity
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_app_dependencies():
     """
-    Configura dependências como tabelas DynamoDB, bucket S3 e SES identity antes dos testes.
+    Configura dependências como tabelas DynamoDB, bucket S3 e SES Identity antes dos testes.
     """
-    with app.lifespan_context():
-        yield
+    # Configure os recursos necessários antes dos testes
+    create_users_table()
+    create_s3_bucket()
+    verify_ses_email_identity()
